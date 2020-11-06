ddf8e064dd64d3f6b81275230921b1f5f85e9d9d
Ryan Kelln <ryan.kelln@gmail.com>
2020-11-05 20:59:24 -0500

    Js refactoring to split splides from trivia

--- public/js/trivia.js
+++ public/js/trivia.js
@@ -1,8 +1,4 @@
 
-const wrap = document.querySelector(".embla");
-const viewPort = wrap.querySelector(".embla__viewport");
-const slideContainer = viewPort.querySelector(".embla__container");
-
 const enableQuestion = (formElement, enable) => {
   if (!formElement || formElement.classList.contains("answered")) { return }
 
@@ -10,58 +6,8 @@ const enableQuestion = (formElement, enable) => {
   inputs.forEach(input => { input.disabled = !enable; });
 }
 
-const createSlidesFromJSON = (jsonData, container) => {
-  let html = '';
-
-  if (!jsonData.hasOwnProperty('slides') || jsonData.slides == null) {
-    console.log("No slide data in js");
-    return;
-  }
-
-  jsonData.slides.forEach((slide, s) => {
-    let question_num = s + 1;
-    html += `
-    <div class="embla__slide">
-      <div class="embla__slide__inner">
-        <img 
-          class="embla__slide__img" 
-          src="data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs%3D"
-          data-src="public/images/${slide.image}" />`;
-
-    if (slide.hasOwnProperty('question') && slide.hasOwnProperty('answers')) {
-      html += `
-            <form id="question${s}" class="question" data-question="${s}" method="post" action="answer/${s}">
-              <fieldset>
-                <legend><span class="number">${question_num}</span>${slide.question}</legend>
-                <ol>`;
-
-      slide.answers.forEach((answer, a) => {
-        let value = a + 1;
-        let name = `s${question_num}a${value}`;
-        html += `
-                  <li>
-                    <button name="answer" id="${name}" value="${value}" disabled>
-                      ${answer}
-                    </buttton>
-                  </li>`;            
-      });
-
-      html += `
-              </ol>
-              <p class="results">
-                <span class="numCorrect"></span> of <span class="numCompleted"></span> guests anwered this correctly.
-              </p>
-            </fieldset>
-          </form>`;
-
-    }
-    html += `
-      </div>
-    </div>
-    `;
-  });
-  container.innerHTML = html;
-
+const createTriviaFromJSON = (jsonData, container) => {
+  
   updateGame(jsonData);
 
   // set up buttons
@@ -112,6 +58,24 @@ const createSlidesFromJSON = (jsonData, container) => {
   );
 };
 
+const initTrivia = data => {
+
+  createTriviaFromJSON(data, slideContainer);
+
+  // enable question form when image is visible
+  embla.on("select", (event) => {
+    let form = slideContainer.querySelector("#question" + embla.selectedScrollSnap());
+    enableQuestion(form, true);
+  });
+
+  // set up first question and scroll to it
+  let form = slideContainer.querySelector(".question:not(.answered)");
+  if (form) {
+    enableQuestion(form, true);
+    embla.scrollTo(form.dataset.question);
+  }
+};
+
 const updateGame = data => {
 
   if (!data.hasOwnProperty('slides') || data.slides == null) {
@@ -158,110 +122,6 @@ const updateGame = data => {
   });
 };
 
-const lazyLoad = embla => {
-  const slides = embla.slideNodes();
-  const images = slides.map(slide => slide.querySelector(".embla__slide__img"));
-  const imagesInView = [];
-
-  const addImageLoadEvent = (image, callback) => {
-    image.addEventListener("load", callback);
-    return () => image.removeEventListener("load", callback);
-  };
-
-  const loadImagesInView = () => {
-    // load images in view
-    embla
-      .slidesInView(true)
-      .filter(index => imagesInView.indexOf(index) === -1)
-      .forEach(loadImageInView);
-    // also load the next image not in view
-    if (imagesInView.length > 0 && imagesInView.length !== images.length) {
-      let last = imagesInView[imagesInView.length-1] + 1;
-      if (last < images.length - 1) {
-        loadImageInView(last);
-      }
-    }
-  };
-
-  const loadImageInView = index => {
-    const image = images[index];
-    const slide = slides[index];
-    imagesInView.push(index);
-    image.src = image.getAttribute("data-src");
-
-    const removeImageLoadEvent = addImageLoadEvent(image, () => {
-      slide.classList.add("has-loaded");
-      removeImageLoadEvent();
-    });
-  };
-
-  return () => {
-    loadImagesInView();
-    return imagesInView.length === images.length;
-  };
-};
-
-const initSlides = data => {
-  createSlidesFromJSON(data, slideContainer);
-  const embla = EmblaCarousel(viewPort, {loop: true});
-  const loadImagesInView = lazyLoad(embla);
-  const loadImagesInViewAndDestroyIfDone = eventName => {
-    const loadedAll = loadImagesInView();
-    if (loadedAll) embla.off(eventName, loadImagesInViewAndDestroyIfDone);
-  };
-
-  embla.on("init", loadImagesInViewAndDestroyIfDone);
-  embla.on("select", loadImagesInViewAndDestroyIfDone);
-  embla.on("resize", loadImagesInViewAndDestroyIfDone);
-
-  // enable question form when image is visible
-  embla.on("select", (event) => {
-    let form = slideContainer.querySelector("#question" + embla.selectedScrollSnap());
-    enableQuestion(form, true);
-  });
-
-  // set up first question and scroll to it
-  let form = slideContainer.querySelector(".question:not(.answered)");
-  if (form) {
-    enableQuestion(form, true);
-    embla.scrollTo(form.dataset.question);
-  }
-
-  // listen to keys
-  document.addEventListener('keyup', (e) => {
-    switch (e.code) {
-      case 'ArrowRight':
-        embla.scrollNext()
-        break;
-      case 'ArrowLeft':
-        embla.scrollPrev()
-        break;
-    }
-  });
-}
-
-/*
-fetch('slides')
-  .then(response => {
-    if (response.ok) {
-      return Promise.resolve(response);
-    }
-    else {
-      return response.text().then(text => {
-        throw new Error(text);
-      });
-    }
-  })
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-    initSlides(data);
-  })
-  .catch(error => {
-    console.error(error);
-    alert(error);
-  });
-*/
 
 // player name dialog
 var playerName = "unknown player";
@@ -295,6 +155,7 @@ const setPlayerName = name => {
     .then(data => {
       //console.log(data);
       initSlides(data);
+      initTrivia(data);
     })
     .catch(error => {
       console.error(error);
